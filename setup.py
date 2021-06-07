fe07acb8a3f391afec2a33037bf08893b4591e1a
bcanfield <brandincanfield@gmail.com>
2021-06-07 14:20:09 -0400

    Add test and small fix to linear generator

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'MMA DFS Toolkit'
 LONG_DESCRIPTION = 'MMA DFS Toolkit written in Python'
 
--- southpaw/tests/test_linear.py
+++ southpaw/tests/test_linear.py
@@ -1,2 +1,10 @@
+from southpaw.generators.linear import LinearGenerator
+
 def test_linear():
-    assert True
+    jsonDataFilePath = 'southpaw/tests/testFighterData.json'
+    salaryCap = 100
+    scoreColumnName = 'score'
+    playersPerLineup = 6
+    numLineupsToGenerate = 10
+    lg = LinearGenerator(jsonDataFilePath, salaryCap, scoreColumnName, playersPerLineup, numLineupsToGenerate)
+    assert len(lg) == 10
