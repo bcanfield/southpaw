3e2098bbe86e9af257162f7f390bb35a52f60f19
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2021-06-07 13:52:21 -0400

    Rename package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'MMA DFS Toolkit'
 LONG_DESCRIPTION = 'MMA DFS Toolkit written in Python'
 
 # Setting up
 setup(
-    name="jab",
+    name="southpaw",
     version=VERSION,
     author="Brandin Canfield",
     author_email="<brandincanfield@gmail.com>",
