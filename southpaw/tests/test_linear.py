171105479ab2638b6420dc3975eac943c23320da
bcanfield <brandincanfield@gmail.com>
2021-06-07 14:37:30 -0400

    Fix broken test

--- southpaw/tests/test_linear.py
+++ southpaw/tests/test_linear.py
@@ -1,10 +1,13 @@
 from southpaw.generators.linear import LinearGenerator
 
+
 def test_linear():
     jsonDataFilePath = 'southpaw/tests/testFighterData.json'
     salaryCap = 100
     scoreColumnName = 'score'
     playersPerLineup = 6
     numLineupsToGenerate = 10
-    lg = LinearGenerator(jsonDataFilePath, salaryCap, scoreColumnName, playersPerLineup, numLineupsToGenerate)
-    assert len(lg) == 10
+    linearGenerator = LinearGenerator(
+        jsonDataFilePath, salaryCap, scoreColumnName, playersPerLineup, numLineupsToGenerate)
+    generatedLineups = linearGenerator.generate()
+    assert len(generatedLineups) == 10
