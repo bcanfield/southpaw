84c2af850192cb8056f6888e4a65010e59a5f9fe
bcanfield <brandincanfield@gmail.com>
2023-10-18 01:16:01 -0400

    fix: Semantic

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '3.0.1'
+VERSION = '3.0.2'
 DESCRIPTION = 'Python Fanduel API'
 LONG_DESCRIPTION = 'Automate your DFS experience with this Python Fanduel API'
 
