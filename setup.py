5b4cd336601859d6f671cfb076ec1b3c1b45f672
Brandin Canfield <brandincanfield@gmail.com>
2023-10-18 00:11:55 -0400

    semantice-release-1 (#58)
    BREAKING CHANGE: semantic release

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Python Fanduel API'
 LONG_DESCRIPTION = 'Automate your DFS experience with this Python Fanduel API'
 
